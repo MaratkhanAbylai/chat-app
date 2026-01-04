d4e7af979c95897db5f4523e95145351cb3e450c
MaratkhanAbylai <m.abylai22@gmail.com>
2026-01-05 01:52:39 +0500

    Вернул начальную страницу на логин

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Main from "./components/Main/Main";
 import styles from "./App.module.css";
 
 function App() {
-  const [screen, setScreen] = useState("main");
+  const [screen, setScreen] = useState("login");
   const [user, setUser] = useState(null);
 
   return (
