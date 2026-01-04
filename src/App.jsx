ce87cbe46016404745445da6ccfbaf07251b5afa
MaratkhanAbylai <m.abylai22@gmail.com>
2026-01-05 02:56:02 +0500

    коммит посреди работы чтобы сделать пулл

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Main from "./components/Main/Main";
 import styles from "./App.module.css";
 
 function App() {
-  const [screen, setScreen] = useState("login");
-  const [user, setUser] = useState(null);
+  const [screen, setScreen] = useState("main");
+  const [user, setUser] = useState("null");
 
   return (
     <div className={styles.app}>
--- src/components/Main/contentComponents/Search/Search.jsx
+++ src/components/Main/contentComponents/Search/Search.jsx
@@ -6,23 +6,40 @@ function Search() {
 
     const [inputValue, setInputValue] = useState('');
     const [resultUsers, setResultUsers] = useState([
-        {id: uuid(), login: "Cole Palmer", link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUtRQOe9JQO3bQetUJsweuxTH_v3pmtp3LVQ&s"}
+        {id: uuid(), login: "Cole Palmer", avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUtRQOe9JQO3bQetUJsweuxTH_v3pmtp3LVQ&s"}
     ]);
+
+    async function search() {
+        if(!inputValue.trim()) return;
+
+        const response = await fetch('/api/users', {
+            method: 'GET',
+            headers: {'Content-type' : 'application/json'},
+            body: {username: inputValue}
+        });
+
+        const res = await response.json();
+
+    }
 
     return  <>
 
         <div className="searchContainer">
 
-            <input type="text" 
-             value={inputValue}
-             onChange={e => setInputValue(e.target.value)}
-            />
+            <div className="input-block">
+                <input type="text" 
+                 value={inputValue}
+                 onChange={e => setInputValue(e.target.value)}
+                />
+
+                <button>Поиск</button>
+            </div>
 
             <div className="result-block">
 
                 {resultUsers.map(user => (
                     <div className="user" key={user.id}>
-                        <img src={user.link} alt="Изоброжения пользователя" />
+                        <img src={user.avatar} alt="Изоброжения пользователя" />
                         <p className='friends-login'>{user.login}</p>
                         <button className='send-request-btn'>Отправить запрос</button>
                     </div>
